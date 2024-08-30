3da9655448bcb41b16762769ab48a6f8d1fa242f
Usman Awan <uawan8154@gmail.com>
2024-08-30 14:07:13 +0100

    .

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const port = 5000;
 app.get("", (req, res)=>{
     res.send("App working fine 🥰");
 })
-app.use("/api", require("./routes/youtube"));
-app.use("/api", require("./routes/facebook"));
-app.use("/api", require("./routes/instagram"));
-app.use("/api", require("./routes/allShortsDownloader"));
+app.use("/api", require("./routes/youtube.js"));
+app.use("/api", require("./routes/facebook.js"));
+app.use("/api", require("./routes/instagram.js"));
+app.use("/api", require("./routes/allShortsDownloader.js"));
 
 app.listen(port, ()=>{
     console.log(`Server running fine on http://localhost:${port}`)
